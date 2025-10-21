c30c21dbd6c7b348bde5eeda8e0a37494957f646
lovebazel2025-arch <lovebazel2025@gmail.com>
2025-10-21 19:07:59 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "nuwaii-stripe",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": { "start": "node server.js" },
+  "dependencies": {
+    "express": "4.19.2",
+    "stripe": "16.6.0",
+    "cors": "2.8.5",
+    "body-parser": "1.20.2"
+  }
+}
